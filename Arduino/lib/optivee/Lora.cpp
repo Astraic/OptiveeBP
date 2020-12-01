eed841d4888bf619edd380658a271eb7461374d5
AdmiraalRuyter <59971971+AdmiraalRuyter@users.noreply.github.com>
2020-12-01 15:40:02 +0100

    lora beta 2
    verbetering + entiteit decode code

--- Arduino/lib/optivee/Lora.cpp
+++ Arduino/lib/optivee/Lora.cpp
@@ -1,4 +1,45 @@
 #include<Lora.hpp>
+StringShift oOutPutBufferData = StringShift();
+
+const unsigned TX_INTERVAL = 60;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+ .nss = 10,
+ .rxtx = LMIC_UNUSED_PIN,
+ .rst = 9,
+ .dio = {2, 6, 7},
+};
+
+const u1_t PROGMEM APPEUI[8] = {0xE5, 0x09, 0x01, 0x00, 0x00, 0xAC, 0x59, 0x00};
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+// This should also be in little endian format, see above.
+const u1_t PROGMEM DEVEUI[8] = {0xBB, 0x04, 0x1b, 0x00, 0x00, 0xAC, 0x59, 0x00};
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+// This key should be in big endi=-=- n format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+// The key shown here is the semtech default key.
+const u1_t PROGMEM APPKEY[16] = {0x92, 0xFD, 0x50, 0xE0, 0xF0, 0x28, 0x11, 0x43, 0xB6, 0x1C, 0x62, 0x57, 0x3A, 0x6B, 0x7C, 0x35};
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+String sAliveString = "0000000000000000";
+char mydata[] = "                         ";
+osjob_t sendjob;
+bool lWaitingResponse = false;
+
+// code SensorString+asingmenttoken+value;
+//      3 chars + 1 char + 8 chars;
+//      tmp=21      ;wgt=100.1    ;
+//
+//
+// static char mydata[] = "                         ";
+// static osjob_t sendjob;
+// bool lWaitingResponse = false;
+
+
 
 void StringShift::shiftOut()
 {
@@ -40,4 +81,132 @@ void StringShift::clear()
         sBuffer[i] = "";
 
     nPointer = 0;
+}
+
+
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+
+            // Disable link check validation (automatically enabled
+            // during join, but not supported by TTN at this time).
+            LMIC_setLinkCheckMode(0);
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.println(F("Received "));
+              Serial.println(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+              lWaitingResponse = false;
+            }
+            // Schedule next transmission
+            // os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void do_send(osjob_t* j)
+{
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } 
+    else if(!lWaitingResponse)
+    {
+        // Prepare upstream data transmission at the next possible time.
+        String sOutputBuffer = oOutPutBufferData.getAndRemoveFirst();
+        if(sOutputBuffer == "")
+            sAliveString.toCharArray(mydata, 25);
+        else
+            sOutputBuffer.toCharArray(mydata, 25);
+         
+
+        LMIC_setTxData2(1, ((xref2u1_t)mydata), sizeof((uint8_t)mydata)-1, 0);
+        // Serial.println(sOutputBuffer);
+        Serial.println(F("Packet queued"));
+        lWaitingResponse = true;
+    }
+}
+
+String UIDTOSTRING(unsigned int UIDID[])
+{
+    String sReturn = "";
+    for(int i = 0; i < 4; i++)
+    {
+        if(UIDID[i] < 0x10)
+            sReturn += "0" + String(UIDID[i], HEX);
+        else
+            sReturn += String(UIDID[i], HEX);
+    }
+    return sReturn;
+}
+
+
+void sendEntityProduction(unsigned int nUID[], int nWeight)
+{
+    oOutPutBufferData.append(String(ENTITYPRODUCTION, HEX) + UIDTOSTRING(nUID) + String(nWeight, HEX));
+    do_send(&sendjob);
+}
+
+void sendEntityFood(unsigned int nUID[], int nWeight)
+{
+    oOutPutBufferData.append(String(ENTITYREGISTRATION, HEX) + UIDTOSTRING(nUID) + String(nWeight, HEX));
+    do_send(&sendjob);
+}
+
+void sendEntityRegistration(unsigned int nUID[], int nWeight, int nTemp)
+{
+    oOutPutBufferData.append(String(ENTITYFOOD, HEX) + UIDTOSTRING(nUID) + String(nWeight, HEX) + String(nTemp, HEX));
+    do_send(&sendjob);
 }
--- Arduino/src/main.ino
+++ Arduino/src/main.ino
@@ -27,55 +27,14 @@
  * Do not forget to define the radio type correctly in config.h.
  *
  *******************************************************************************/
-
 #include <Arduino.h>
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
 #include <Lora.hpp>
 
-#define ALIVETIME 600000
-
-const unsigned TX_INTERVAL = 60;
-
-// Pin mapping
-const lmic_pinmap lmic_pins = {
- .nss = 10,
- .rxtx = LMIC_UNUSED_PIN,
- .rst = 9,
- .dio = {2, 6, 7},
-};
-
-String sAliveString = "LIS";
-static const u1_t PROGMEM APPEUI[8]= {0xE5, 0x09, 0x01, 0x00, 0x00, 0xAC, 0x59, 0x00};
-void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
-
-// This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]= {0xBB, 0x04, 0x1b, 0x00, 0x00, 0xAC, 0x59, 0x00};
-void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
-
-// This key should be in big endi=-=- n format (or, since it is not really a
-// number but a block of memory, endianness does not really apply). In
-// practice, a key taken from ttnctl can be copied as-is.
-// The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = {0x92, 0xFD, 0x50, 0xE0, 0xF0, 0x28, 0x11, 0x43, 0xB6, 0x1C, 0x62, 0x57, 0x3A, 0x6B, 0x7C, 0x35};
-void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
-
-// code SensorString+asingmenttoken+value;
-//      3 chars + 1 char + 8 chars;
-//      tmp=21      ;wgt=100.1    ;
-//
-//
-
-
-StringShift oOutPutBufferData = StringShift();
-
-static char mydata[] = "                         ";
-static osjob_t sendjob;
-
-bool lWaitingResponse = false;
 unsigned long nTimer = 0;
-
+unsigned int uid[] = {24, 24, 24, 24};
 void setup() {
     Serial.begin(115200);
     Serial.println(F("Starting"));
@@ -95,6 +54,9 @@ void setup() {
     // Start job (sending automatically starts OTAA too)
     nTimer = millis();
     do_send(&sendjob);
+
+
+
 }
 
 void loop() 
@@ -106,7 +68,9 @@ void loop()
         nTimer = millis();
     }
 
-
+    sendEntityProduction(uid, 100);             // Send data for product enity
+    sendEntityFood(uid, 100);                   // Send data for food enity
+    sendEntityRegistration(uid, 100, 21);       // Send data for registation enity
 
 
 }
