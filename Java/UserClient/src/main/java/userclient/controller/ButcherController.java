7388e890d4ac2bcee1fc50e9b85b5d5618de8517
Stephan de Jongh <stephandejongh@hotmail.nl>
2021-01-20 04:28:45 +0100

    Staafdiagrammen + style
    Staafdiagrammen toegevoegd en bijbehorende stijlklassen toegepast.

--- Java/UserClient/src/main/java/userclient/controller/ButcherController.java
+++ Java/UserClient/src/main/java/userclient/controller/ButcherController.java
@@ -5,8 +5,18 @@
  */
 package userclient.controller;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import userclient.model.Quality;
 
 /**
  *
@@ -14,10 +24,21 @@ import javafx.fxml.FXML;
  */
 public class ButcherController extends SuperController {
     
+    @FXML
+    protected BarChart<String, Double> barChartCategory, barChartMeat, barChartFat;
+    
+    private final ArrayList<Quality> dataset;
+    
     public ButcherController() {
-        
+        dataset = qualityClient.select();
     }
     
+    public void initialize() {
+        barChartCategory.setData(getData(categoryClient.select()));
+        barChartMeat.setData(getData(meatClient.select()));
+        barChartFat.setData(getData(fatClient.select()));
+    }
+
     @FXML
     private void selectAnimal(ActionEvent event){
         
@@ -26,5 +47,35 @@ public class ButcherController extends SuperController {
     @FXML
     private void confirmQuality (ActionEvent event){
         
+    }  
+           
+    private ObservableList<Series<String, Double>> getData(ArrayList qualifiers) {
+        ObservableList<Series<String, Double>> data = FXCollections.observableArrayList();
+        Series<String, Double> series = new Series<>();
+        
+        data.add(series);
+        
+        qualifiers.forEach(q -> {
+            double amount = dataset.stream().filter(hasQualifier(q)).mapToDouble(c -> c.getAmount()).sum();
+            series.getData().add(new Data<>(q.toString(), amount));
+        });
+        
+        return data;
+    }
+    
+    private Predicate<Quality> hasQualifier(Object qualifier) {
+        for(Method m : Quality.class.getMethods()) {
+            if(qualifier.getClass().equals(m.getReturnType())){
+                return p -> {
+                    try {
+                        return qualifier.toString().equalsIgnoreCase(m.invoke(p).toString());
+                    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+
+                    }
+                    return false;
+                };
+            }
+        }
+        return null;
     }
 }
