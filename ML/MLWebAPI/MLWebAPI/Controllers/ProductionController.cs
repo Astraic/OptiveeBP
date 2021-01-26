08debe5e66107f12723121d9ee26ec263725fe7c
Thijs Rijpert <60793766+thijsrijpert@users.noreply.github.com>
2021-01-26 07:32:47 -0800

    Uploaded ML.NET Solution

---
+++ ML/MLWebAPI/MLWebAPI/Controllers/ProductionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MLWebAPIML.Model;
+using System;
+using System.Text.Json;
+using System.Web.Script.Serialization;
+
+namespace MLWebAPI.Controllers
+{
+    [ApiController]
+    public class ProductionController : ControllerBase
+    {
+
+        private readonly ILogger<ProductionController> _logger;
+
+        public ProductionController(ILogger<ProductionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public float Get()
+        {
+            ModelInput data = JsonSerializer.Deserialize<ModelInput>(Request.Query["json"]);
+
+            data.Date = DateTime.Now.ToString("dd/mm/yyyy HH:mm:ss");
+            data.Productiondatetime = DateTime.Now.AddDays(1).ToString("dd/mm/yyyy HH:mm:ss");
+            // Make a single prediction on the sample data and print results
+            ModelOutput predictionResult = ConsumeModel.Predict(data);
+            return (Int32) predictionResult.Score;
+        }
+    }
+}
