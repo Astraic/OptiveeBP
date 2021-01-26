6e86d8f86cc11c336198e09b4e4f9e935a84ff81
Stephan de Jongh <stephandejongh@hotmail.nl>
2021-01-26 16:37:20 +0100

    FeedPredictionML

---
+++ ML/FeedPrediction/Controllers/PredictController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.ML;
+using FeedPrediction.DataModel;
+
+namespace FeedPrediction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictController : ControllerBase
+    {
+
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public PredictController(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<float> Post([FromBody] ModelInput modelInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            ModelOutput prediction = _predictionEnginePool.Predict(modelName: "FeedAnalysisModel", example: modelInput);
+
+            float production = prediction.Score;
+
+            return Ok(production);
+        }
+
+    }
+}
---
+++ ML/FeedPrediction/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.ML;
+using FeedPrediction.DataModel;
+
+namespace FeedPrediction
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddPredictionEnginePool<ModelInput, ModelOutput>().FromFile(modelName: "FeedAnalysisModel", filePath: "MLModel.zip", watchForChanges: true);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
